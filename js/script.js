e5128c6c913f0d9b942d8a6efcd83abf075daf64
Brenda-A-S <brenda.antunes16@gmail.com>
2023-10-27 18:05:50 -0300

    first commit: add contador completo

---
+++ js/script.js
@@ -0,0 +1,30 @@
+import Countdown from './modules/countdown.js';
+
+const halloweenTime = new Countdown('30 October 2023 23:59:59 GMT-0300')
+
+function updateCountdown() {
+    const units = {
+        days: 'dia',
+        hours: 'hora',
+        minutes: 'minuto',
+        seconds: 'segundo'
+    }
+    function formatTime(number, unit) {
+        const unitString = number === 1 ? unit : unit + 's';
+        return `${number} <span class="subtitle">${unitString}</span>`;
+    }
+    const daysElement = document.querySelector('.days');
+    const hoursElement = document.querySelector('.hours');
+    const minutesElement = document.querySelector('.minute');
+    const secondsElement = document.querySelector('.second');
+    daysElement.innerHTML = formatTime(halloweenTime.total.days, units.days);
+    hoursElement.innerHTML = formatTime(halloweenTime.total.hours, units.hours);
+    minutesElement.innerHTML = formatTime(halloweenTime.total.minutes, units.minutes);
+    secondsElement.innerHTML = formatTime(halloweenTime.total.seconds, units.seconds);
+}
+
+updateCountdown();
+
+setInterval(updateCountdown, 1000);
+
+
