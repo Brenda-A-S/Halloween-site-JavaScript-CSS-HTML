4f14744b0f2e5faa955ac4c1d34f2215cd410f81
Brenda-A-S <brenda.antunes16@gmail.com>
2023-11-03 15:25:42 -0300

    feat: adição de novas páginas(home e jogar) em construção

---
+++ js/modules/engine.js
@@ -0,0 +1,63 @@
+const views = {
+    squares: document.querySelectorAll('.square'),
+    enemy: document.querySelector('.enemy'),
+    time: document.querySelector('#time'),
+    score: document.querySelector('#score')
+}
+const { squares, enemy, time, score } = views;
+
+const values = {
+    gameVelocity: 1000,
+    hitPosition: 0,
+    result: 0,
+    currentTime: 60
+}
+const { gameVelocity } = values;
+let { hitPosition, result, currentTime } = values;
+
+const action = {
+    timerId: setInterval(selectRandomSquare, gameVelocity),
+    countdownTimer: setInterval(countdown, gameVelocity)
+}
+
+let {timerId, countdownTimer} = action;
+
+function countdown() {
+    currentTime--;
+    time.textContent = currentTime;
+    if(currentTime <= 0){
+        alert('game over, score: ' + result);
+        clearInterval(timerId, gameVelocity);
+        clearInterval(countdownTimer, gameVelocity);
+        score.textContent = result;
+        window.location.reload();
+    }
+}
+
+function selectRandomSquare() {
+    squares.forEach(square => square.classList.remove('enemy'));
+    let randomNum = Math.floor(Math.random() * 9);
+    hitPosition = randomNum;
+    squares[randomNum].classList.add('enemy');
+}
+
+function verifyEnemy() {
+    squares.forEach(square => {
+        square.addEventListener('mousedown', () => {
+            if (hitPosition === +square.id) {
+                result++;
+                score.textContent = result;
+                hitPosition = null;
+                selectRandomSquare();
+                clearInterval(timerId);
+                timerId = setInterval(selectRandomSquare, gameVelocity);
+            }
+        });
+    });
+}
+
+function initGame() {
+    verifyEnemy();
+}
+
+initGame()
--- js/script.js
+++ js/script.js
@@ -1,30 +1,38 @@
-import Countdown from './modules/countdown.js'
+import Countdown from './modules/countdown.js';
+import Tooltip from './modules/tooltip.js';
 
-const halloweenTime = new Countdown('31 October 2024 00:00:00 GMT-0300')
 
-function updateCountdown() {
-    const units = {
-        days: 'dia',
-        hours: 'hora',
-        minutes: 'minuto',
-        seconds: 'segundo'
+if(window.location.pathname.includes('countdown')){
+    const halloweenTime = new Countdown('31 October 2024 00:00:00 GMT-0300')
+    
+    function updateCountdown() {
+        const units = {
+            days: 'dia',
+            hours: 'hora',
+            minutes: 'minuto',
+            seconds: 'segundo'
+        }
+        function formatTime(number, unit) {
+            const unitString = number === 1 ? unit : unit + 's'
+            return `${number} <span class="subtitle">${unitString}</span>`
+        }
+        const daysElement = document.querySelector('.days')
+        const hoursElement = document.querySelector('.hours')
+        const minutesElement = document.querySelector('.minute')
+        const secondsElement = document.querySelector('.second')
+        daysElement.innerHTML = formatTime(halloweenTime.total.days, units.days)
+        hoursElement.innerHTML = formatTime(halloweenTime.total.hours, units.hours)
+        minutesElement.innerHTML = formatTime(halloweenTime.total.minutes, units.minutes)
+        secondsElement.innerHTML = formatTime(halloweenTime.total.seconds, units.seconds)
     }
-    function formatTime(number, unit) {
-        const unitString = number === 1 ? unit : unit + 's'
-        return `${number} <span class="subtitle">${unitString}</span>`
-    }
-    const daysElement = document.querySelector('.days')
-    const hoursElement = document.querySelector('.hours')
-    const minutesElement = document.querySelector('.minute')
-    const secondsElement = document.querySelector('.second')
-    daysElement.innerHTML = formatTime(halloweenTime.total.days, units.days)
-    hoursElement.innerHTML = formatTime(halloweenTime.total.hours, units.hours)
-    minutesElement.innerHTML = formatTime(halloweenTime.total.minutes, units.minutes)
-    secondsElement.innerHTML = formatTime(halloweenTime.total.seconds, units.seconds)
+    
+    updateCountdown()
+    
+    setInterval(updateCountdown, 1000)
 }
 
-updateCountdown()
+if(window.location.pathname.includes('smashpumpkin')){
 
-setInterval(updateCountdown, 1000)
+}
 
-
+Tooltip()
