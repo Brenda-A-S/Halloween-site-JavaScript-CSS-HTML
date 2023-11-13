2fa643394bcd0d3c26f0cedfcc350414231fc3c0
Brenda-A-S <brenda.antunes16@gmail.com>
2023-11-13 13:34:28 -0300

    feat: add função de trocar som

--- src/js/modules/engine.js
+++ src/js/modules/engine.js
@@ -18,12 +18,27 @@ export default class SmashGame {
 
         this.clearIntervals = this.clearIntervals.bind(this);
         this.resetCurrentInterval = this.resetCurrentInterval.bind(this);
+        this.playSound = this.playSound.bind(this);
         this.countdown = this.countdown.bind(this);
         this.selectRandomSquare = this.selectRandomSquare.bind(this);
         this.verifyEnemy = this.verifyEnemy.bind(this);
         this.openResult = this.openResult.bind(this);
         this.startGame = this.startGame.bind(this);
         this.initGame = this.initGame.bind(this);
+    }
+    clearIntervals() {
+        clearInterval(this.countdownTimer);
+        clearInterval(this.timerId);
+        clearInterval(this.timerId);
+    }
+    resetCurrentInterval() {
+        clearInterval(this.timerId);
+        this.timerId = setInterval(this.selectRandomSquare, this.gameSpeed);
+    }
+    playSound(audioName) {
+        let audio = new Audio(`src/assets/sounds/${audioName}.mp3`);
+        audio.volume = 0.2;
+        audio.play();
     }
     countdown() {
         if (this.currentTime > 0) {
@@ -46,15 +61,16 @@ export default class SmashGame {
             this.squares[randomNum].classList.add('enemy');
         }
     }
-    verifyEnemy() {
+    verifyEnemy() {        
         if (this.gameRunning) {
             return;
         }
 
         this.gameRunning = true;
         this.squares.forEach(square => {
-            square.addEventListener('click', () => {
+            square.addEventListener('click', () => {                
                 if (this.hitPosition === +square.id) {
+                    this.playSound('punch');
                     if (this.currentTime > 0) {
                         this.result++;
                     }
@@ -70,16 +86,8 @@ export default class SmashGame {
         this.modal.newHTMLModal("Fim de Jogo", 'Pontuação: ' + this.result, 'Jogar Novamente!')
         this.modal.container.classList.add('active')
     }
-    clearIntervals() {
-        clearInterval(this.countdownTimer);
-        clearInterval(this.timerId);
-        clearInterval(this.timerId);
-    }
-    resetCurrentInterval() {
-        clearInterval(this.timerId);
-        this.timerId = setInterval(this.selectRandomSquare, this.gameSpeed);
-    }
     startGame() {
+        this.playSound('time');
         this.verifyEnemy();
         this.currentTime = 15;
         score.textContent = 0;
